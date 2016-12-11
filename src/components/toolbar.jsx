27161e0e85a53b24db16b63791a50d6ebb14f8b0
Nikolay Karev <karev.n@gmail.com>
2016-12-11 17:59:40 +0500

    Toolbar refactoring & fix

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,14 +1,41 @@
 require('./toolbar.sass')
 import React from 'react'
 import fullscreen from '../fullscreen'
-import share from '../share'
+import {getShareUrl} from '../share'
 
 const hasFullscreen = fullscreen.supports()
+
+function ShareMenu(props) {
+  return (
+    <div className="reactbox-share-menu">
+      { Object.keys(props.services).map((slug)=> {
+        const service = props.services[slug]
+        return (
+          <a target="_blank" className="reactbox-share-link"
+            key={slug}
+            onClick={(e)=> {e.preventDefault();
+              props.dispatch('share.close'); false}}
+            href={getShareUrl(service, props.activeItem)}>
+            <If condition={service.icon}>
+              <img src={service.icon} alt="" />
+            </If>
+            <If condition={!service.icon}>
+              <i className={`reactbox-icon-${slug}`} />
+            </If>
+            {service.name}
+          </a>
+        )
+      })
+      }
+    </div>
+  )
+}
 
 export default class Toolbar extends React.Component {
   constructor (props) {
     super(props)
     this.state = {shareOpen: false}
+    this.onWindowClickWhenSharing = ::this.onWindowClickWhenSharing
   }
   componentWillUpdate (newProps, newState) {
     if (newState.shareOpen && !this.state.shareOpen){
@@ -32,75 +59,54 @@ export default class Toolbar extends React.Component {
   render (props = this.props) {
     const activeItem = props.items[props.activeIndex]
     return (
-      <div className="reactbox-toolbar-wrapper">
-        <div className="reactbox-toolbar">
-          <div className="reactbox-toolbar-actions">
-            <a href="#" onClick={(e)=> {e.preventDefault(); props.dispatch('unmount');}}
-              className="reactbox-toolbar-close reactbox-toolbar-link">
-              <i className="reactbox-icon-close" />
-            </a>
-            <If condition={hasFullscreen && !props.toolbar.isFullscreen}>
-              <a href="#" onClick={(e)=> {e.preventDefault(); props.dispatch('fullscreen.enter');}}
-                className="reactbox-toolbar-fullscreen reactbox-toolbar-link">
-                <i className="reactbox-icon-fullscreen" />
-                <span className="reactbox-tooltip">
-                  {props.i18n.toolbar.fullscreen.enter}
-                </span>
-              </a>
-            </If>
-            <If condition={hasFullscreen && props.toolbar.isFullscreen}>
-              <a href="#"
-                className="reactbox-toolbar-link reactbox-toolbar-exit-fullscreen"
-                onClick={(e)=> {e.preventDefault(); props.dispatch('fullscreen.exit');}}>
-                <i className="reactbox-icon-exit-fullscreen" />
-                <span className="reactbox-tooltip">
-                  {props.i18n.toolbar.fullscreen.exit}</span>
-              </a>
-            </If>
-            <If condition={activeItem && Object.keys(props.services).length > 0 &&
-               props.toolbar.share}>
-              <div className="reactbox-toolbar-share">
-                <i className="reactbox-icon-share"
-                  onClick={(e)=> {e.preventDefault();
-                    this.setState('shareOpen': true); props.dispatch('share.open'); }}></i>
-                <span className="reactbox-tooltip">
-                  {props.i18n.toolbar.share}</span>
-                <If condition={props.toolbar.shareActive}>
-                  <div className="reactbox-share-menu">
-                    { Object.keys(props.services).map((slug)=> {
-                      const service = props.services[slug]
-                      return (
-                        <a target="_blank" className="reactbox-share-link"
-                          key={slug}
-                          onClick={(e)=> {e.preventDefault();
-                            props.dispatch('share.close'); false}}
-                          href={share.getShareUrl(service, activeItem)}>
-                          <If condition={service.icon}>
-                            <img src={service.icon} alt="" />
-                          </If>
-                          <If condition={!service.icon}>
-                            <i className={"reactbox-icon-#{slug}"} />
-                          </If>
-                          {service.name}
-                        </a>
-                      )
-                    })
-                    }
-                  </div>
-                </If>
-              </div>
-            </If>
-            <If condition={activeItem.download}>
-              <a className="reactbox-toolbar-download reactbox-toolbar-link"
-                download href={activeItem.download}
-                target="_blank">
-                <i className="reactbox-icon-download" />
-                <span className="reactbox-tooltip">
-                  {props.i18n.toolbar.download}</span>
-              </a>
+      <div className="reactbox-toolbar">
+        <a href="#" onClick={(e)=> {e.preventDefault(); props.dispatch('unmount');}}
+          className="reactbox-toolbar-close reactbox-toolbar-link">
+          <i className="reactbox-icon-close" />
+        </a>
+        <If condition={hasFullscreen && !props.toolbar.isFullscreen}>
+          <a href="#" onClick={(e)=> {e.preventDefault(); props.dispatch('fullscreen.enter');}}
+            className="reactbox-toolbar-fullscreen reactbox-toolbar-link">
+            <i className="reactbox-icon-fullscreen" />
+            <span className="reactbox-tooltip">
+              {props.i18n.toolbar.fullscreen.enter}
+            </span>
+          </a>
+        </If>
+        <If condition={hasFullscreen && props.toolbar.isFullscreen}>
+          <a href="#"
+            className="reactbox-toolbar-link reactbox-toolbar-exit-fullscreen"
+            onClick={(e)=> {e.preventDefault(); props.dispatch('fullscreen.exit');}}>
+            <i className="reactbox-icon-exit-fullscreen" />
+            <span className="reactbox-tooltip">
+              {props.i18n.toolbar.fullscreen.exit}</span>
+          </a>
+        </If>
+        <If condition={activeItem && Object.keys(props.services).length > 0 &&
+           props.toolbar.share}>
+          <div className="reactbox-toolbar-share">
+            <i className="reactbox-icon-share"
+              onClick={(e)=> {
+                e.preventDefault();
+                this.setState({'shareOpen': true});
+                props.dispatch('share.open'); }}></i>
+            <span className="reactbox-tooltip">
+              {props.i18n.toolbar.share}</span>
+            <If condition={props.toolbar.shareActive}>
+              <ShareMenu dispatch={props.dispatch}
+                services={props.services} activeItem={activeItem} />
             </If>
           </div>
-        </div>
+        </If>
+        <If condition={activeItem.download}>
+          <a className="reactbox-toolbar-download reactbox-toolbar-link"
+            download href={activeItem.download}
+            target="_blank">
+            <i className="reactbox-icon-download" />
+            <span className="reactbox-tooltip">
+              {props.i18n.toolbar.download}</span>
+          </a>
+        </If>
       </div>
     )
   }
