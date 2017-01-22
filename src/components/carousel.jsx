90061640405c7c9f797f14dd2e8346884bac392d
Nikolay Karev <karev.n@gmail.com>
2017-01-22 22:48:53 +0500

    Stateless components in carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,84 +8,79 @@ import CloseIcon from 'react-icons/md/close'
 import css from '../css'
 import find from 'array.prototype.find'
 import {get as property} from 'dot-prop'
+import curry from 'curry'
 
 function differs(a, b, props) {
   return find(props, (prop) => property(a, prop) !== property(b, prop))
 }
 
-class Item extends React.Component {
-  constructor (props) {
-    super(props)
-    this.onImageMounted = ::this.onImageMounted
-    this.onClick = ::this.onClick
-    this.onError = ::this.onError
-    this.onLoad = ::this.onLoad
-  }
-  onLoad (e) {
-    this.props.item.thumbnailSize = {width: e.target.naturalWidth,
+const onClick = curry(function onClick(props, e) {
+  e.preventDefault()
+  props.dispatch('item.thumbnail.click', props.item)
+})
+const onError = curry(function onError(props, e) {
+  props.dispatch('item.thumbnail.error', props.item)
+})
+const onLoad = curry(function onLoad(props, e) {
+  props.item.thumbnailSize = {width: e.target.naturalWidth,
       height: e.target.naturalHeight}
-    this.props.dispatch('item.thumbnail.load', this.props.item)
+  props.dispatch('item.thumbnail.load', props.item)
+})
+const onImageMounted = curry(function onImageMounted(props, image) {
+  if (!image) {
+    return
   }
-  onError (e) {
-    this.props.dispatch('item.thumbnail.error', this.props.item)
+  if (image.complete || props.item.thumbnailLoaded) {
+    props.dispatch('item.thumbnail.load', props.item)
   }
-  onClick (e) {
-    e.preventDefault()
-    this.props.dispatch('item.thumbnail.click', this.props.item)
-  }
-  onImageMounted (image) {
-    if (!image) {
-      return
-    }
-    if (image.complete || this.props.item.thumbnailLoaded) {
-      this.props.dispatch('item.thumbnail.load', this.props.item)
-    }
-  }
-  componentWillUnmount () {
-    this.props.item.thumbnailLoaded = false
-  }
-
-  render (props = this.props) {
-    const imageStyle =
-      css.prefix({transform: `translate(${props.left}px, 0)`})
-    const classes = classnames('reactbox-carousel-item', {
-      'reactbox-active': props.item.index === props.activeIndex,
-      'reactbox-loaded': props.item.thumbnailLoaded || props.item.thumbnailError,
-      'reactbox-error': props.item.thumbnailError,
-      'reactbox-animated': props.item.thumbnailLoaded || props.item.thumbnailError
-    })
-    return (
-      <div className={classes} onClick={this.onClick} style={imageStyle}>
-        <If condition={!props.item.error}>
-          <img src={props.item.thumbnail}
-            onLoad={this.onLoad}
-            onError={this.onError}
-            ref={this.onImageMounted} />
-        </If>
-        <If condition={props.item.error}><CloseIcon /></If>
-      </div>
-    )
-  }
+})
+function thumbnailTransform(props) {
+  return {transform: translate(props.left, props.top)} }
+function translate(x, y) {
+  return `translate(${x}px, ${y || 0}px)` }
+function Item (props) {
+  const imageStyle = css.prefix(thumbnailTransform(props))
+  const classes = classnames('reactbox-carousel-item', {
+    'reactbox-active': props.item.index === props.activeIndex,
+    'reactbox-loaded': props.item.thumbnailLoaded || props.item.thumbnailError,
+    'reactbox-error': props.item.thumbnailError,
+    'reactbox-animated': props.item.thumbnailLoaded || props.item.thumbnailError
+  })
+  return (
+    <div className={classes} onClick={onClick(props)} style={imageStyle}>
+      <If condition={!props.item.error}>
+        <img src={props.item.thumbnail}
+          onLoad={onLoad(props)}
+          onError={onError(props)}
+          />
+      </If>
+      <If condition={props.item.error}><CloseIcon /></If>
+    </div>
+  )
 }
+
+function getItemWidth(props, item) {
+  if (!item.thumbnail || item.thumbnailError) {
+      return 100
+    }
+  if (!item.thumbnailSize) {
+    return 0
+  }
+  return props.carousel.height *
+    item.thumbnailSize.width / item.thumbnailSize.height
+}
+
+
 
 export default class Carousel extends React.Component {
   constructor (props) {
     super(props)
     this.onWindowResize = ::this.onWindowResize
   }
-  getWidth (item) {
-    if (!item.thumbnail || item.thumbnailError) {
-      return 100
-    }
-    if (!item.thumbnailSize) {
-      return 0
-    }
-    return this.props.carousel.height *
-      item.thumbnailSize.width / item.thumbnailSize.height
-  }
+
   getLeftForActive () {
     return window.innerWidth / 2 -
-      this.getWidth(this.props.items[this.props.activeIndex]) / 2
+      getItemWidth(this.props, this.props.items[this.props.activeIndex]) / 2
   }
   componentDidMount () {
     this.updateSize()
@@ -107,7 +102,7 @@ export default class Carousel extends React.Component {
     if (current.index < this.props.items.length - 1) {
       for (let i = current.index + 1; i < this.props.items.length; i++) {
         const item = props.items[i]
-        left = left + this.getWidth(props.items[i - 1]) + 12
+        left = left + getItemWidth(props, props.items[i - 1]) + 12
         visible.push({item: item, left: left})
         if (!(item.thumbnailSize &&
           (item.thumbnailLoaded || item.thumbnailError)) ||
@@ -121,10 +116,10 @@ export default class Carousel extends React.Component {
       current.thumbnailError)) {
       for (let i = current.index - 1; i >= 0; i--) {
         const item = this.props.items[i]
-        left = left - (this.getWidth(this.props.items[i])) - 12
+        left = left - (getItemWidth(props, this.props.items[i])) - 12
         visible.unshift({item: item, left: left})
         if (!(item.thumbnailSize && (item.thumbnailLoaded || item.thumbnailError)) ||
-          item.left < -(window.innerWidth + this.getWidth(item))) {
+          item.left < -(window.innerWidth + getItemWidth(props, item))) {
           break
         }
       }
